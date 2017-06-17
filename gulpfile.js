3fa27785fa3449920673a920466078d299359bba
ouyangxin1992 <1310815076@qq.com>
2017-06-17 09:44:08 +0800

    reset

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+//引入gulp
+var gulp=require("gulp"),
+    livereload=require("gulp-livereload"),
+    webserver=require("gulp-webserver"),
+    sass=require("gulp-ruby-sass"),
+    uglify=require("gulp-uglify"),
+    imagemin=require("gulp-imagemin"),
+    pngquant=require("imagemin-pngquant"),
+    jshint=require("gulp-jshint"),
+    rename=require("gulp-rename"),
+    sourcemaps=require("gulp-sourcemaps");
+
+gulp.task("webserver",function () {
+    gulp.src("./dist").
+    pipe(webserver({
+        livereload:true,
+        open:true
+    }));
+});
+
+gulp.task("html",function () {
+    return gulp.src("src/**/*.html").
+    pipe(gulp.dest("dist/"))
+});
+
+gulp.task("sass",function () {
+    return sass("src/sass/**/*.scss",{style:"compact"})
+        .on("error",function (err) {
+            console.log("编译sass出错%s",err.message)
+        })
+        .pipe(gulp.dest("dist/css/"))
+});
+gulp.task("js",function () {
+    return gulp.src(["src/js/**/*.","!*.min.js"])
+        .pipe(sourcemaps.init())
+        .pipe(rename({suffix:".min"}))
+        .pipe(uglify())
+        .pipe(gulp.dest("dist/js"));
+});
+gulp.task("script",function () {
+    gulp.src("./src/js/**/*.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter("default"))
+});
+gulp.task("imagemin",function(){
+    return gulp.src("src/images/**/*.{png,jpg,gif,svg}")
+        .pipe(imagemin({
+            progressive:true,
+            svgoPlugins:[{removeViewBox:false}],
+            use:[pngquant()]
+        }))
+        .pipe(gulp.dest("dist/images"))
+});
+
+gulp.task("default",["sass","js","imagemin","html","webserver"])
